01f0ac73e25407f944d21bc15fe38ff6aa13dc59
shepherd869 <shepherd8@gmail.com>
2020-07-31 12:28:47 -0500

    Updates

---
+++ precache-manifest.a35b64e57c0bc87205e1e310d4c4ae22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2c829444fa67e67c64d4914c9e2361",
+    "url": "/keep-clone/index.html"
+  },
+  {
+    "revision": "4e9b0960f456221e18c9",
+    "url": "/keep-clone/static/css/main.2ea92b38.chunk.css"
+  },
+  {
+    "revision": "52f32f5f5d72eae11578",
+    "url": "/keep-clone/static/js/2.0bae657d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keep-clone/static/js/2.0bae657d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9b0960f456221e18c9",
+    "url": "/keep-clone/static/js/main.96301cc4.chunk.js"
+  },
+  {
+    "revision": "b1003ca23fb9afc41364",
+    "url": "/keep-clone/static/js/runtime-main.2e53d94b.js"
+  }
+]);
